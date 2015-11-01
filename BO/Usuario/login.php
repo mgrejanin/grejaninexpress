f19deee3f6deaba78affcc6672b4481c219f07d9
Mauricio <junior310595@gmail.com>
2015-11-01 12:56:08 -0200

    Tyestetetetetet

--- BO/Usuario/login.php
+++ BO/Usuario/login.php
@@ -2,11 +2,7 @@
 /*
  * Arquivo para validação de login de usuário
  */
-$teste= __DIR__ .'/../../Model/UsuarioDAO.php';
-echo $teste;
-exit();
-
-// include_once $teste;
+include_once '/../../Model/UsuarioDAO.php';;
 
 if ($_SERVER ['REQUEST_METHOD'] == "POST") {
 	if (UsuarioDAO::ValidarLogin ( $_POST ['ds_email'], $_POST ['ds_senha'] )) {
