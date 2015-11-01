d690ec85aa12e2d3f1bdec5a83f58f465be87018
Mauricio <junior310595@gmail.com>
2015-11-01 13:09:38 -0200

    Update Origem

--- BO/Origem/insert.php
+++ BO/Origem/insert.php
@@ -1,5 +1,5 @@
 <?php
-include_once ("/home/mgrejanin1/public_html/grejaninexpress/Model/FreteDAO.php");
+include_once ("/home/mgrejanin1/public_html/grejaninexpress/Model/OrigemDAO.php");
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 	if (OrigemDAO::inserirOrigem($_POST)){
 		header("Location:../../fretes.php");
