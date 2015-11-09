7941606df4b53e3de8f41c327bf755eeb337275b
Mauricio <junior310595@gmail.com>
2015-11-09 09:23:07 -0200

    Atualização da página de login

--- index.php
+++ index.php
@@ -22,51 +22,41 @@ if (isset ( $_GET ['action'] )) {
 <html>
 <head>
 	<title>Login | Grejanin Express</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="login_files/formoid1/formoid-solid-dark.css" type="text/css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css" >
+        <link rel="stylesheet" href="bootstrap/css/signin.css" >
 </head>
-<body class="blurBg-true" style="background-color:#FFFFFF;">
-
-
-
-<!-- Start Formoid form-->
-<script type="text/javascript" src="login_files/formoid1/jquery.min.js"></script>
-<form class="formoid-solid-dark" style="margin-top: 10%;background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px;" 
-method="post" action="BO/Usuario/login.php">
-	<div class="title">
-		<h2>Login</h2>
-	</div>
-	
-	<!-- Campo Email -->
-	<div class="element-input">
-		<label class="title">
-			<span class="required">*</span>
-		</label>
-		<div class="item-cont">
-			<input class="large" type="email" name="ds_email" required="required" placeholder="email"/>
-			<span class="icon-place"></span>
-		</div>
-	</div>
-	<!-- Campo Email -->
-	
-	<!-- Campo Senha -->
-	<div class="element-password">
-		<label class="title">
-			<span class="required">*</span>
-		</label>
-		<div class="item-cont">
-			<input class="large" type="password" name="ds_senha" required="required" placeholder="Senha"/>
-			<span class="icon-place"></span>
-		</div>
-	</div>
-	<!-- Campo Senha -->
-	
-	<!-- BotÃ£o Login -->
-	<div class="submit">
-		<input type="submit" value="Login"/>
-	</div>
-</form><script type="text/javascript" src="login_files/formoid1/formoid-solid-dark.js"></script>
-<!-- Stop Formoid form-->
-
+<body>
+<div class="container">
+    <!-- Formulário Bootstrap-->
+    <form class="form-signin" method="post" action="BO/Usuario/login.php">
+    
+    	<h2 class="form-signin-heading">Login</h2>
+    	
+        <!-- Campo Email -->
+            <label for="ds_email" class="sr-only">Email</label>
+            <input class="form-control" type="email" name="ds_email" id="ds_email" required="" autofocus="" placeholder="Digite seu e-mail">
+        <!-- ./Campo Email -->
+        
+        <!-- Campo senha -->
+            <label for="ds_senha" class="sr-only">Senha</label>
+            <input class="form-control" type="password" name="ds_senha" id="ds_senha" required="" placeholder="Digite sua senha">
+        <!-- ./Campo senha -->
+        
+        <div class="checkbox">
+        	<label>	
+	        	<input type="checkbox" value="1">
+	        	Mantenha-me conectado
+        	</label>
+        </div>
+        
+        <!-- Botão Enviar -->
+        <button type="submit" class="btn btn-lg btn-primary btn-block">Logar</button>
+        <!-- ./Botão Enviar -->
+        
+    </form>
+    <!-- ./Formulário Bootstrap-->
+</div>
 </body>
 </html>
