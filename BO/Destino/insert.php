b3999bbfb921ef8b85b87dd747a8e74130e7a243
Mauricio Junior <junior310595@gmail.com>
2015-11-25 09:50:26 -0200

    Rollback

--- BO/Destino/insert.php
+++ BO/Destino/insert.php
@@ -1,5 +1,6 @@
 <?php
-include_once ("/home/mgrejanin1/public_html/grejaninexpress/Model/DestinoDAO.php");
+// include_once ("/home/mgrejanin1/public_html/grejaninexpress/Model/DestinoDAO.php");
+include_once '../../Model/DestinoDAO.php';
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 	if (DestinoDAO::inserirDestino($_POST)){
 		header("Location:../../cadastrarDestino.php?action=success");
