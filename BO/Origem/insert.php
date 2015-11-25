cc34112378b3292ef5c8be1c84e44a322c0bf5ee
Mauricio Junior <junior310595@gmail.com>
2015-11-25 11:52:32 -0200

    Atualizações no formulário de cadastro de origem

--- BO/Origem/insert.php
+++ BO/Origem/insert.php
@@ -2,8 +2,8 @@
 include_once ("/home/mgrejanin1/public_html/grejaninexpress/Model/OrigemDAO.php");
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 	if (OrigemDAO::inserirOrigem($_POST)){
-		header("Location:../../fretes.php");
+		header("Location:../../cadastrarOrigem.php?action=success");
 	}
 } else  {
-	echo "deu ruim";
+	header("Location:../../cadastrarOrigem.php?action=error");
 }
--- cadastrarOrigem.php
+++ cadastrarOrigem.php
@@ -1,71 +1,91 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Cadastro de Origem | Grejanin Express</title>
-	<link rel="stylesheet" href="formoid_files/formoid1/formoid-solid-dark.css" type="text/css" />	
-	<script type="text/javascript" src="formoid_files/formoid1/jquery.min.js"></script>
-	
-	<!-- Bootstrap core CSS -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    
-	 <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Cadastro de Origem | Grejanin Express</title>
 </head>
-<body class="blurBg-false" style="background-color: #EBEBEB">
+<body>
 
 <?php include_once 'topo.php';?>
+	
+	<div class="container">
+		<h3>Cadastro de Origem</h3>
+		<hr>
+		<form action="BO/Origem/insert.php" method="post">
+		
+			<!-- Campo nome -->
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-4">
+						<label for="nm_origem">Nome:</label> 
+						<input type="text" name="nm_origem" class="form-control" id="nm_origem" placeholder="Nome da Origem" required="required" >
+					</div>
+				</div>
+			</div>
+			<!-- ./Campo nome -->
+			
+			<br>
+			
+			<!-- Campo Sigla -->
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-2">
+						<label for="ds_sigla">Sigla:</label> 
+						<input type="text" class="form-control" name="ds_sigla" id="ds_sigla" placeholder="Sigla da Origem" required="required">
+					</div>
+				</div>
+			</div>
+			<!-- ./Campo Sigla -->
+			
+			<br>
+			
+			<!-- Campo Observação -->
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-4">
+						<label for="ds_obs">Observação:</label>
+						<textarea class="form-control" rows="3" cols="10" placeholder="Observações sobre a Origem"></textarea>
+					</div>
+				</div>
+			</div>
+			<!-- ./Campo Observação -->
+			
+			<br>
+			
+			<!-- Botão enviar -->
+			<button type="submit" class="btn btn-success">Cadastrar</button>
+			<!-- ./Botão enviar -->
+			
+		</form>		
+		
+		<?php 
+			if (isset($_GET['action'])):?>
+			
+			<br>
+			<div class="row">
+				<div class="col-md-4">
+				
+					<?php if ($_GET['action'] == "error"){ ?>
+						<div class="alert alert-danger alert-dismissible" role="alert">
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<strong>Erro ao cadastrar origem!</strong>
+						</div>
+					<?php } elseif ($_GET['action'] == "success"){ ?>
+						<div class="alert alert-success alert-dismissible" role="alert">
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<strong>Origem cadastrada com sucesso!</strong>
+						</div>
+					<?php } ?>
+				</div>
+			</div>			
+		<?php endif;?>
+		
+	</div>
 
-	<!-- Start Formoid form-->	
-	<script type="text/javascript"
-		src="cadastrarfuncionario_files/formoid1/jquery.min.js"></script>
-	<form class="formoid-solid-dark"
-		style="background-color: #FFFFFF; font-size: 14px; font-family: 'Roboto', Arial, Helvetica, sans-serif; color: #34495E; max-width: 480px; min-width: 150px"
-		action="BO/Origem/insert.php" method="post">
-		<div class="title">
-			<h2>Cadastro de Origem</h2>
-		</div>
-		
-		 <!-- Nome origem -->
-		<div class="element-input">
-			<label class="title"><span class="required">*</span></label>
-			<div class="item-cont">
-				<input class="large" type="text" name="nm_origem" required="required"
-					placeholder="Nome" /><span class="icon-place"></span>
-			</div>
-		</div>
-		<!-- Nome origem -->
-		
-		<!-- Sigla Origem -->
-		<div class="element-input">
-			<label class="title"><span class="required">*</span></label>
-			<div class="item-cont">
-				<input class="large" type="text" name="ds_sigla" required="required"
-					placeholder="Sigla da Origem" /><span class="icon-place"></span>
-			</div>
-		</div>
-		<!-- Sigla Origem -->
-		
-		<!-- Observação -->
-		<div class="element-input">
-			<label class="title"></label>
-			<div class="item-cont">
-				<input class="large" type="text" name="ds_obs"
-					placeholder="Observação" /><span class="icon-place"></span>
-			</div>
-		</div>
-		<!-- Observação -->
-		
-		<!-- Botão Enviar -->
-		<div class="submit">
-			<input type="submit" value="Cadastrar" />
-		</div>
-		<!-- Botão Enviar -->
-		
-	</form>
+	<!-- Footer -->
+	<?php include_once 'rodape.php';?>
+	<!-- /Footer -->
 
 </body>
 </html>
