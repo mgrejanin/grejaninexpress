ce1e6af91b3a2c1db5bef8f057adc26591143e61
Mauricio Junior <junior310595@gmail.com>
2015-11-25 09:48:56 -0200

    Caminho de arquivo no arquivo insert.php

--- BO/Destino/insert.php
+++ BO/Destino/insert.php
@@ -1,6 +1,5 @@
 <?php
-// include_once ("/home/mgrejanin1/public_html/grejaninexpress/Model/DestinoDAO.php");
-include_once '../../Model/DestinoDAO.php';
+include_once ("/home/mgrejanin1/public_html/grejaninexpress/Model/DestinoDAO.php");
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 	if (DestinoDAO::inserirDestino($_POST)){
 		header("Location:../../cadastrarDestino.php?action=success");
